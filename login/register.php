22fefaa628ecfe2945dc19259d77d103fe9d7b94
interly <0311interly@gmail.com>
2014-04-30 17:44:03 +0800

    modified:   register.php

--- login/register.php
+++ login/register.php
@@ -16,11 +16,9 @@
 	$pass_word = $_SERVER['HTTP_PASSWORD'];
 	$SQL_CHECK_DUPLICATE = "SELECT * FROM `User` WHERE `username` = '$user_name'";
 	$result = mysqli_query($db, $SQL_CHECK_DUPLICATE);
-	while($temp = mysqli_fetch_row($result))
-	{
-		echo $temp;
-	}
-	if($result == NULL)
+	$user_id = mysqli_fetch_row($result);
+	echo $user_id;
+	if($result)
 	{
 		while($temp = mysqli_fetch_row($result))
 		{
