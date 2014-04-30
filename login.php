e1c312afe0c702fbf57dd90ec067349e6ac64317
Edward <huangxx_2155@qq.com>
2014-04-30 17:21:00 +0800

    make the html content in login.php heredoc

--- login.php
+++ login.php
@@ -1,29 +1,39 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Login</title>
-	<script type="text/javascript" src="login/login.js"/>
-</head>
-<body style="font-family:verdana, sans-serif;">
-<h2>Login</h2>
+<?php
+	#Check for cookie,
+	# if cookie exist and correct, auto redirect to room selection
+	#otherwise
+	$html_content = <<<EOD
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Login</title>
+		<script type="text/javascript" src="login/login.js"/>
+	</head>
+	<body style="font-family:verdana, sans-serif;">
+	<h2>Login</h2>
 
 
-<form method=post action="login/auth.php">
-<table cellspacing=5pt cellpadding=10pt style="border:1pt solid #7777ff;">
-	<tr>
-	<td>User</td>
-	<td><input type="text" size=32 id="username"></td>
-	</tr>
+	<form method=post action="login/auth.php">
+	<table cellspacing=5pt cellpadding=10pt style="border:1pt solid #7777ff;">
+		<tr>
+		<td>User</td>
+		<td><input type="text" size=32 id="username"></td>
+		</tr>
 
-	<tr>
-	<td>Password</td>
-	<td><input type="password" size=32 id="password"></td>
-	</tr>
+		<tr>
+		<td>Password</td>
+		<td><input type="password" size=32 id="password"></td>
+		</tr>
 
-	<tr>
-	<td><button type="button">Login</button></td>
-	</tr>
-</table>
-<a href="register.html">click to register</a>
-</body>
-</html>
+		<tr>
+		<td><button type="button">Login</button></td>
+		</tr>
+	</table>
+	<a href="register.html">click to register</a>
+	</body>
+	</html>
+EOD;
+	echo $html_content;
+?>
+
+
