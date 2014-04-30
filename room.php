639c8ca4a8681b3e7b4c7a8507b0590cf5de6623
Sylvia <sylvia@haha.com>
2014-04-30 16:54:36 +0800

    test format

--- room.php
+++ room.php
@@ -18,13 +18,13 @@
         </div>
         <div id="Info">
             <div id="userInfo">
-                <table id="userInfoTable" class="infoTable" border="3">
+                <table id="userInfoTable" class="infoTable">
                     <tr><td>what</td><td>what????????</td></tr>
                 </table>
             </div>
             <div id="roomInfo">
                 <table id="roomInfoTable" class="infoTable">
-                    <tr></tr>
+                   <tr><td>what the hell</td><td>what????????</td></tr>
                 </table>
             </div> 
         </div>
