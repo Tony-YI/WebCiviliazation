c1676b059c6a20bd0f99d05374dd5ffc7bc0eb0e
interly <0311interly@gmail.com>
2014-05-01 16:51:14 +0800

    modified:   room.js

--- room/room.js
+++ room/room.js
@@ -6,13 +6,64 @@ var current_usrname = $.cookie("CURRENT_USERNAME");
 var in_room = 0;
 
 if(current_usr_id)
-	console.log("CURRENT_USER is " + current_usr_id);
+	console.log("CURRENT_USER is " + current_usr);
 
 function room_get_list()
 {
-	//This function to get the existing room data from the server 
-	//send request to room/get_room.php, and then dynamically render the page
+	//This function is to send request to room/get_room.php
+	//Then get the existing room data from the server and dynamically render the page
+	var xhr = new XMLHttpRequest();
+	xhr.setRequestHeader('refresh_type',"true");
+	xhr.open("POST","room/get_room.php",true);
+	xhr.send();
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4)
+		{
+			if(xhr.status != 200)
+			{
+				alert("Error code = " + new String(xhr.status));
+				return false;
+			}
+			var room_info = xhr.responseText;
+			document.getElementById("roomNum").innerHTML = room_info;
+		}
+	};
+	return false;
 }
+
+function pseudo_room_get_list()
+{
+	//This function is to send request and current room data in the browser to room/get_room.php
+	//If room data in server updated, then get the existing room data from the server and dynamically render the page
+	//Else do nothing
+	var current_room = document.getElementsByClassName("roomBtn");
+	var current_room_info = "";
+	for (var i = 0; i < current_room.length; i++)
+	{
+		current_room_info = current_room_info + current_room.id;
+	}
+	console.log(current_room_info);
+	var xhr = new XMLHttpRequest();
+	xhr.setRequestHeader('refresh_type',"true");
+	xhr.setRequestHeader('refresh_type',"true");
+	xhr.open("POST","room/get_room.php",true);
+	xhr.send();
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4)
+		{
+			if(xhr.status != 200)
+			{
+				alert("Error code = " + new String(xhr.status));
+				return false;
+			}
+			var room_info = xhr.responseText;
+			document.getElementById("roomNum").innerHTML = room_info;
+		}
+	};
+	return false;
+}
+setInterval(room_get_list,10000);
+setInterval(pseudo_room_get_list,1000);
 
 function room_onclick()
 {	
@@ -24,7 +75,7 @@ function room_create()
 	var xhr = new XMLHttpRequest();
 	//this is a synchornous request
 	xhr.open("POST","room/create_room.php",false);
-	xhr.setRequestHeader("USERID",current_usr_id);
+	xhr.setRequestHeader("USERID",current_usr);
 	xhr.send();
 	console.log(xhr.responseText);
 	response = JSON.parse(xhr.responseText);
