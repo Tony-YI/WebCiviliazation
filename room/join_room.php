cb01df751ffd78ec7b463fbf3b2bdb265f506014
interly <0311interly@gmail.com>
2014-05-03 00:37:57 +0800

    £º	modified:   room/join_room.php

--- room/join_room.php
+++ room/join_room.php
@@ -10,5 +10,62 @@
 		"CREATE TABLE slots_1...."
 		"CREATE TABLE players_1...."
 	*/
+	require_once("../lib/db.php");
+	$response = "";
+	if(check_cookie($db))
+	{
+		//If the cookie exists
+		$user_id = $_SERVER['HTTP_USERID'];
+		$room_id = $_SERVER['HTTP_ROOMID'];
+		
+		$SQL_CHECK_ROOM = "SELECT * FROM Game WHERE Game_id = '$room_id'";
+		$result = mysqli_query($db, $SQL_CHECK_ROOM);
+		if(mysqli_num_rows($result) == 0)
+		{
+			$response['status'] = 'room not exist';
+		}
+		
+		$SQL_CHECK_FULL = "SELECT P1, P2, P3 FROM Game WHERE Game_id = '$room_id'";
+		$result = mysqli_query($db, $SQL_CHECK_FULL);
+		$seat = mysqli_fetch_row($result);
+		if($seat[0] && $seat[1] && $seat[2])
+		{
+			$response['status'] = 'room full';
+		}
+		
+		$SQL_CHECK_USER = "SELECT * FROM Game WHERE P1 = '$user_id' OR P2 = '$user_id' OR P3 = '$user_id'";
+		$result = mysqli_query($db, $SQL_CHECK_USER);
+		if(mysqli_num_rows($result) != 0)
+		{
+			$response['status'] = 'user not available';
+		}
+		
+		if($response['status'] == "")
+		{
+			$response['status'] = 'success';
+			$SQL_ROOM_INFO = "SELECT P1, P2, P3 FROM Game WHERE Game_id = '$room_id'";
+			$result = mysqli_query($db, $SQL_ROOM_INFO);
+			$seat = mysqli_fetch_row($result);
+			$SQL_JOIN_ROOM = "";
+			if(!$seat[0])
+			{
+				$SQL_JOIN_ROOM = "UPDATE Game SET P1 = '$user_id' WHERE Game_id = '$room_id'";
+			}
+			else if(!$seat[1])
+			{
+				$SQL_JOIN_ROOM = "UPDATE Game SET P2 = '$user_id' WHERE Game_id = '$room_id'";
+			}
+			else if(!$seat[2])
+			{
+				$SQL_JOIN_ROOM = "UPDATE Game SET P3 = '$user_id' WHERE Game_id = '$room_id'";
+			}
+		}
+	}
+	else
+	{
+		$response['status'] = 'failed';
+	}
+	$response = json_encode($response);
+	echo $response;
 	
 ?>
