cbfd90280377d524f29c554f900cd885421791ab
Sylvia <sylvia@haha.com>
2014-05-02 15:28:11 +0800

    formating

--- room.php
+++ room.php
@@ -38,11 +38,11 @@
                 {
                    $num = $row[0];
                     //echo "<button type='submit' class='roomBtn' id=$num>Room $num ! Click to enter room</button>";
-                   echo """<div class='roomBtn' id=$num>
+                   echo "<div class='roomBtn' id=$num>
                                     <div class='roomNumLine'>Room $num ! Click to enter button to enter! </div>
                                     <button type='submit' class='enterBtn' id='enterRoom'$num>ENTER</button>
                                     <div class='roomInfoLine'>hahahaha hehehehe huhuhuhuhu gugugugugu</div>
-                                    </div>"""
+                                    </div>"
                     if($num % 2 == 0)
                     {
                         echo "<br><br>";
