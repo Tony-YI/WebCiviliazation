b929a71575c99d3c32ae2979ab27a0993b7e387b
Sylvia <sylvia@haha.com>
2014-05-02 14:10:07 +0800

    formating

--- room.php
+++ room.php
@@ -50,10 +50,10 @@
         </div>
         
         <div id="Info" align="right">
-            
+            <div id="main" align="right">
             <button type="button" id="logout" onclick="logout()">logout</button>
             <button type="button" id="newRoom" onclick="room_create()">create a new room</button>
-            
+            </div>
             <div id="userInfo">
                 <table id="userInfoTable" border="3px">
                     <tr><td>what</td><td>what????????</td></tr>
