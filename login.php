e8afa05feb7fdf0f3e79abe902d0ece90e265be4
tony-yi <455054421@qq.com>
2014-05-02 16:59:17 +0800

    modified /login/login.css by Tony

--- login.php
+++ login.php
@@ -19,23 +19,28 @@
 </head>
 <body style="font-family:verdana, sans-serif;">
 <h2>Welcome To WebCivilization</h2>
-<form method=post action="login/auth.php">
-	<table cellspacing=5pt cellpadding=10pt style="border:1pt solid #7777ff;">
-		<tr>
-		<td>User</td>
-		<td><input type="text" size=32 id="username"></td>
-		</tr>
 
-		<tr>
-		<td>Password</td>
-		<td><input type="password" size=32 id="password"></td>
-		</tr>
+<div name="form">
+	<form method=post action="login/auth.php">
+		<table cellspacing=5pt cellpadding=10pt>
+			<tr>
+				<td>User</td>
+				<td><input type="text" size=32 id="username"></td>
+			</tr>
 
-		<tr>
-		<td><button type="button" onclick="login()">Login</button></td>
-		</tr>
-	</table>
-	<a href="register.html">click to register</a>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" size=32 id="password"></td>
+			</tr>
+
+			<tr>
+				<td><button type="button" onclick="login()">LogIn</button></td>
+			</tr>
+		</table>
+	</form>
+		<a href="register.html">click to register</a>
+</div>
+
 </body>
 </html>
 
