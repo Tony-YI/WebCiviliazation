efb7d2388e25ed3868c0588412b694cf1dc536cb
interly <0311interly@gmail.com>
2014-05-02 21:24:18 +0800

    modified:   room/room.js

--- room/room.js
+++ room/room.js
@@ -62,21 +62,21 @@ function isUpade(browser_games, server_games)
 				var server = server_games[i];
 				console.log(browser);
 				console.log(server);
-				console.log(browser.p1);
+				console.log(browser.getAttribute('p1'));
 				console.log(server.P1);
-				if(browser.p1 != server.P1)
+				if(browser.getAttribute('p1') != server.P1)
 				{
 					return true;
 				}
-				console.log(browser.p2);
+				console.log(browser.getAttribute('p2'));
 				console.log(server.P2);
-				if(browser.p2 != server.P2)
+				if(browser.getAttribute('p2') != server.P2)
 				{
 					return true;
 				}
-				console.log(browser.p3);
+				console.log(browser.getAttribute('p3'));
 				console.log(server.P3);
-				if(browser.p3 != server.P3)
+				if(browser.getAttribute('p3') != server.P3)
 				{
 					return true;
 				}
