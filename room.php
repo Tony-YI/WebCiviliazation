a70c084a84206b7645e9d31cce4a21cb08e032eb
Sylvia <sylvia@haha.com>
2014-05-03 18:33:26 +0800

    width

--- room.php
+++ room.php
@@ -62,9 +62,7 @@
                   
                    
                     //echo "<button type='submit' class='roomBtn' id=$num>Room $num ! Click to enter room</button>";
-                    echo "<div class='roomBtn' id=$num onclick=room_onclick()><div class='numDiv' id='room$num'>Room $num ! Click  and Enter this room ! !</div>
-                    <div class='gameInfo' id='room$numInfo' P1='$p1' P2='$p2' P3='$p3'><br>Player1 : $p1Name<br>Player2 : $p2Name<br>Player3 : $p3Name<br></div>
-                    </div>";
+                    echo "<div class='roomBtn' id=$num onclick=room_onclick()><div class='numDiv' id='room$num'>Room $num ! Click  and Enter this room ! !</div><div class='gameInfo' id='room$numInfo' P1='$p1' P2='$p2' P3='$p3'><br>Player1 : $p1Name<br>Player2 : $p2Name<br>Player3 : $p3Name<br></div></div>";
                     if($num % 3 == 0)
                     {
                         echo "<br><br>";
