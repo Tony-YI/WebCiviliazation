5869bba9d25189e05e40485b0211ebf09a0b32b2
Edward <huangxx_2155@qq.com>
2014-05-03 19:45:24 +0800

    fix logic bug in room/join_room.php

--- room/join_room.php
+++ room/join_room.php
@@ -148,7 +148,10 @@
 
 		for($count = 0;$count < 3;$count++)
 		{
-			$SQL_INSERT_playerlist = "INSERT INTO game_{$game_id}_playerlist VALUES ($p[0],'$p_name[0]',0,0)";
+			$SQL_INSERT_playerlist = "INSERT INTO game_{$game_id}_playerlist ";
+			$SQL_INSERT_playerlist .= "VALUES (".$p[$count].",";
+			$SQL_INSERT_playerlist .= "'".$p_name[$count]."',";
+			$SQL_INSERT_playerlist .= "0,0)";
 			if(!mysqli_query($con,$SQL_INSERT_playerlist))
 				$response["SQL_INSERT_playerlist_$count"] = mysqli_error($con);
 		}
