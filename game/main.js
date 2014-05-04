09622ca8902434b0dc76d56f941369f9a925b763
Edward <huangxx_2155@qq.com>
2014-05-04 16:57:51 +0800

    1. game/net/init.php now will reply with some basic game information about the game 2. game/main.js contain the inilization of the game page

--- game/main.js
+++ game/main.js
@@ -3,10 +3,18 @@
 */
 function Init()
 {
-	var panel_layer = create_panel_layer();
-	document.body.appendChild(panel_layer);	
+	//send request to get the initilization data
+	Init_get_init_data();
 }
 
+function Init_get_init_data()
+{
+	//This function will send request to get the initilization data
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST","./net/init.php",false);
+	xhr.send();
+	console.log(xhr.responseText);
+}
 //This is the function to add 'ACTIONS' to the form that is going to be processed on the server side
 function player_add_action_to_form(actions)
 {
