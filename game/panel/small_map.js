07b8f79b1a7e13d7d536853187d7a2bb8618dc18
tony-yi <455054421@qq.com>
2014-05-04 18:39:55 +0800

    coding game panel

---
+++ game/panel/small_map.js
@@ -0,0 +1,46 @@
+function init_small_map(e)
+{
+	//20 x 20 map//
+	var slot_num = 484;
+	var line_num = 22; //the top/left/bottom/right are useless
+
+	var slot = Array();
+	for(var i = 0; i < slot_num; i++)
+	{
+		slot[i] = document.createElement('div');
+		slot[i].setAttribute('class', 'slot');
+		var x = i % line_num;
+		var y = parseInt(i / line_num);
+		slot[i].setAttribute('x', x);
+		slot[i].setAttribute('y', y);
+
+		if(x == 0) //start slot
+		{
+			if(y % 2 == 0) //odd begin slot
+			{
+				slot[i].style.opacity = '0.0';
+			}
+			else //even begin slot
+			{
+				slot[i].style.opacity = '0.0';
+			}
+		}
+		else if (x == line_num - 1) //end slot of one row
+		{
+			slot[i].style.opacity = '0.0';
+		}
+	}
+
+	var small_map = document.getElementById('small_map');
+	small_map.addEventListener('contextmenu', no_contextmenu, false);
+
+	for(var i = 0; i < line_num; i++)
+	{
+		for(var j = 0; j < line_num; j++)
+		{
+			box.appendChild(slot[line_num * i + j]);
+		}
+	}
+}
+
+window.addEventListener('load', init_small_map, false);
