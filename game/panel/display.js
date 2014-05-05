fff2c1291d87611992dfd6866266a91c089caa8a
tony-yi <455054421@qq.com>
2014-05-05 16:55:05 +0800

    coding /game by Tony

--- game/panel/display.js
+++ game/panel/display.js
@@ -656,7 +656,7 @@ function addBoxes()
 
 	//TODO: var slot_array = ;
 	var user_id = getCookie('CURRENT_USER'); //in game/game_logic_client/check_functions.js
-	alert(user_id);
+	//alert(user_id);
 
 	var hexagon = Array();
 	for(var i = 0; i < hexagon_num; i++)
