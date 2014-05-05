0032dbf85e7383435e9cc3c30493647dd45d3429
tony-yi <455054421@qq.com>
2014-05-05 19:42:49 +0800

    coding /game by Tony

--- game/panel/display.js
+++ game/panel/display.js
@@ -641,9 +641,8 @@ function resize()
 
 function addBoxes(list)
 {
-	var hehe = list;
-	alert(list.length);
-	alert(slot_list.length);
+	console.log(list.length);
+	console.log(slot_list.length);
 	//20 x 20 map//
 	var hexagon_num = 484;
 	var new_line_num = 22; //the top/left/bottom/right are useless
