f0f7efe6594b96a8800bae21096360b50350e945
Edward <huangxx_2155@qq.com>
2014-05-05 17:14:18 +0800

    rename slot as Slot

--- game/game_logic_client/slot.js
+++ game/game_logic_client/slot.js
@@ -1,6 +1,6 @@
 //This file contains implementation of slot class
 
-function slot(slot_x,slot_y)
+function Slot(slot_x,slot_y)
 {
 	this.x = slot_x;
 	this.y = slot_y;
@@ -13,7 +13,7 @@ function init_slot_list(slots)
 {
 	for(var count = 0;count < slots.length;count++)
 	{
-		var slot = new slot(slots[count].slot_x,slots[count].slot_y);
+		var slot = new Slot(slots[count].slot_x,slots[count].slot_y);
 		slot.type_id = slots[count].slot_type;
 		slot.army_id = slots[count].slot_army;
 		slot.owner = slots[count].slot_owner;
