b764aa89ca9b475da3f040dc89b0abe1188471cc
tony-yi <455054421@qq.com>
2014-05-05 16:48:07 +0800

    coding /game by Tony

--- game/game_logic_client/check_functions.js
+++ game/game_logic_client/check_functions.js
@@ -6,4 +6,16 @@ function check_slot_owner(e) //check the slot owner
 	//TODO: check whether this slot belongs to the user
 	return true;
 }
+
+function getCookie(cname) //get a specific cooike variable
+{
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) 
+  	{
+  		var c = ca[i].trim();
+  		if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  	}
+	return "";
+}
 /***********************************/
