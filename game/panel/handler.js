c1387b4632fc7e3dd89ab237b454bb69d80f38a6
Edward <huangxx_2155@qq.com>
2014-05-05 20:31:54 +0800

    add instructions in finishing functions in handler.js

--- game/panel/handler.js
+++ game/panel/handler.js
@@ -2,12 +2,39 @@
 //variable latest_slot points to the DOM that is the grand-parent of all these actions
 function attack_handler()
 {
+	/*
+	1. show the attack range
+	2. attach attack event listener (attack_action() to the div) 
+	*/
+}
+
+function attack_action()
+{
+	/*
+	1. compute the result of the attack
+	2. show the result
+	3. record the result in result list
+	*/
 }
 
 function move_handler()
 {
+	/*
+	1. show the move range
+	2. attach move event listener (move_action() to the div) 
+	*/
+}
+
+function move_action()
+{
+	/*
+	1. compute the result of the movement
+	2. show the result
+	3. record the result in result list
+	*/
 }
 
 function defend_handler()
 {
+
 }
