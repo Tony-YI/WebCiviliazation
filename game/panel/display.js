305556abd4492981a2921ed7200b280c2e503793
tony-yi <455054421@qq.com>
2014-05-06 21:29:22 +0800

    coding /game by Tony

--- game/panel/display.js
+++ game/panel/display.js
@@ -276,10 +276,7 @@ function set_function_attribute_1(hexagon, function_type)
 	hexagon.removeEventListener('mousedown', mousedown_1, false);
 	hexagon.removeEventListener('mouseup', mouseup_1, false);
 	hexagon.addEventListener('mousedown', mousedown_2, false);
-	if(function_type != 'cancel')
-	{
-		hexagon.addEventListener('mouseover', end_slidein, false);
-	}
+	hexagon.addEventListener('mouseover', end_slidein, false);
 	hexagon.setAttribute('class', 'manual');
 }
 
@@ -294,10 +291,7 @@ function set_function_attribute_2(hexagon, function_type)
 	hexagon.removeEventListener('mousedown', mousedown_1, false);
 	hexagon.removeEventListener('mouseup', mouseup_1, false);
 	hexagon.addEventListener('mousedown', mousedown_3, false);
-	if(function_type != 'back')
-	{
-		hexagon.addEventListener('mouseover', end_slidein, false);
-	}
+	hexagon.addEventListener('mouseover', end_slidein, false);
 	hexagon.setAttribute('class', 'manual');
 }
 
