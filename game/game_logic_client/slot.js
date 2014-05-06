caeae88b8d7fb82dccd8571ae755d3f03a9e014f
Edward <huangxx_2155@qq.com>
2014-05-06 14:43:53 +0800

    debugging

--- game/game_logic_client/slot.js
+++ game/game_logic_client/slot.js
@@ -31,11 +31,12 @@ function init_slot_list(slots)
 			army_object.position_y = slot.slot_y;
 			//console.log(slot);
 		}
-		if(slot.owner_id != "" && slot.type_id == 4) 
+		slot.owner = slots[count].slot_owner;
+		if(slot.owner != "" && slot.type_id == 4) 
 		{
 			console.log(slot);
 			try{
-				var player_object = getPlayerByID(slot.owner_id);
+				var player_object = getPlayerByID(slot.owner);
 				player_object.capital_x = slot.slot_x;
 				player_object.capital_y = slot.slot_y;
 			}
@@ -44,9 +45,6 @@ function init_slot_list(slots)
 				console.log(e);
 			}
 		}
-		//console.log("getting slotowner");
-		slot.owner = slots[count].slot_owner;
-		//console.log("pushing into the array");
 		slot_list.push(slot);
 	}
 }
