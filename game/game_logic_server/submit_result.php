fab499f799bf50ac4298a4701f9c106f6d11b387
Edward <huangxx_2155@qq.com>
2014-05-06 21:22:24 +0800

    debugging

--- game/game_logic_server/submit_result.php
+++ game/game_logic_server/submit_result.php
@@ -31,23 +31,30 @@
 	{
 		$entityBody = file_get_contents('php://input');
 		echo $entityBody."\n";
-		$request = json_decode($entityBody);
+		try
+		{
+			$request = json_decode($entityBody,TRUE);
+		}
+		catch (Exception $e)
+		{
+			echo 'json_decode->Caught exception: ',  $e->getMessage(), "\n";
+		}
 		try
 		{
 			$result_list = $request['result_list'];
 		}
 		catch (Exception $e)
 		{
-			echo 'Caught exception: ',  $e->getMessage(), "\n";
+			echo 'accessing by name Caught exception: ',  $e->getMessage(), "\n";
 		}
-		
+
 		try
 		{
 			echo var_dump($result_list)."\n";
 		}
 		catch (Exception $e)
 		{
-			echo 'Caught exception: ',  $e->getMessage(), "\n";
+			echo 'var_dump() Caught exception: ',  $e->getMessage(), "\n";
 		}
 			
 	}
