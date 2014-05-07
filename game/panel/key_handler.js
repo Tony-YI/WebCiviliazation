0ac15a76191ca63b9f50f3582e3f7cc3fab0ef47
Sylvia <sylvia@haha.com>
2014-05-07 21:33:40 +0800

    change content

--- game/panel/key_handler.js
+++ game/panel/key_handler.js
@@ -1,6 +1,6 @@
 function init_help()
 {
-	document.getElementById('help').innerHTML='<p id="title"><h3>Hint</h3><p>Use key <h4><b>"i"</b></h4> to invoke/hide information board<br/>Use key <h4><b>"m"</b></h4> to invoke/hide small map<br/>Use key <h4><b>"q"</b></h4> to quit game<br/>Use key <h4><b>"h"</b></h4> to invoke/hide hint board</p>';
+	document.getElementById('help').innerHTML='<h3>Hint</h3>Use key <b>"i"</b> to invoke/hide information board<br/>Use key <b>"m"</b> to invoke/hide small map<br/>Use key <b>"q"</b> to quit game<br/>Use key <b>"h"</b> to invoke/hide hint board</p>';
 }
 
 function key_down(e)
