8cf4867e3c32d17d19027d5c1b9d99351e42d90b
Edward <huangxx_2155@qq.com>
2014-05-07 19:24:29 +0800

    debugging

--- game/game_logic_server/query_turn.php
+++ game/game_logic_server/query_turn.php
@@ -12,10 +12,10 @@
 		echo "\"error_detail\":\"no_valid_cookie\"}";
 		exit;
 	}
-	$row = mysqli_fetch_row($result);
+	
 	$SQL_GET_ACTIVE_PLAYER = "SELECT player_id FROM game_{$game_id}_playerlist WHERE player_turn = 1";
 	$result = mysqli_query($db,$SQL_GET_ACTIVE_PLAYER);
-	
+	$row = mysqli_fetch_row($result);
 
 	echo "{\"active_player\":\"$row[0]\"";
 
