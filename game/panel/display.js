d9d0df03f72c474b6c3b69c751316afa60fb41cb
tony-yi <455054421@qq.com>
2014-05-07 15:32:12 +0800

    coding /game by Tony

--- game/panel/display.js
+++ game/panel/display.js
@@ -834,7 +834,11 @@ function go_to_home()
 
 function update_slot() //TODO: update the slot every movement
 {
-
+	//var result_list; global variable
+	if(result_list)
+	{
+		if(result_list)
+	}
 }
 
 
