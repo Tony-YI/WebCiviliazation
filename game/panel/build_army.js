ea0cf440fed3d273fd20539ffa9c242a6640bd50
Sylvia <sylvia@haha.com>
2014-05-07 14:31:08 +0800

    test alert

--- game/panel/build_army.js
+++ game/panel/build_army.js
@@ -25,22 +25,22 @@ function build_army(armyType)
 	switch(armyType)
 	{
 		case 1:
-			if(window.current_player.gold < 200 || current_player.wood < 200){
+			if(window.current_player.gold < 20 || current_player.wood < 20){
 				alert("You do not have enough Gold or Wood to build a sword army");
 			}
-			alert(armyType);
+			
 			break;
 		case 2:
 			if(window.current_player.gold < 25 || current_player.wood < 15){
 				alert("You do not have enough Gold or Wood to build a sword army");
 			}
-			alert(armyType);
+			
 			break;
 		case 3:
 			if(window.current_player.gold < 15 || current_player.wood < 25){
 				alert("You do not have enough Gold or Wood to build a sword army");
 			}
-			alert(armyType);
+			
 			break;
 		default:
 			break;
