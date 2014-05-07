a623f248ad576b2e6d54291a1cd09d3bd8b70517
tony-yi <455054421@qq.com>
2014-05-07 15:36:13 +0800

    coding /game by Tony

--- game/panel/display.js
+++ game/panel/display.js
@@ -236,54 +236,6 @@ function end_slidein(e)  //handel the animation
 	target.setAttribute('type', 'none');
 }
 
-function key_down(e)
-{
-	e.stopPropagation();
-	e.preventDefault();
-
-	var key = e.keyCode;
-	console.log(key);
-	if(key == 77 || key == 73 || key == 81) //m, i, q, h
-	{
-		e.target.addEventListener('keyup', key_up, false);
-	}
-}
-
-function key_up(e)
-{
-	var key = e.keyCode;
-	if(key == 77) // m
-	{
-		e.target.removeEventListener('keyup', key_up, false);
-		clean_small_map_dot(); //in small_map.js
-		if(document.getElementById('small_map').style.display == 'none')
-		{
-			document.getElementById('small_map').style.display = 'block';
-		}
-		else
-		{
-			document.getElementById('small_map').style.display = 'none';
-		}
-	}
-	if(key == 73) // i
-	{
-		e.target.removeEventListener('keyup', key_up, false);
-		if(document.getElementById('info').style.display == 'none')
-		{
-			document.getElementById('info').style.display = 'block';
-		}
-		else
-		{
-			document.getElementById('info').style.display = 'none';
-		}
-	}
-	if(key == 81) // q
-	{
-		e.target.removeEventListener('keyup', key_up, false);
-		alert('quit');
-	}
-}
-
 function no_contextmenu(e) //no default right click when event on the map
 {
 	e.preventDefault();
@@ -838,6 +790,9 @@ function update_slot() //TODO: update the slot every movement
 	if(result_list)
 	{
 		if(result_list)
+		{
+			
+		}
 	}
 }
 
