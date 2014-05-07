0057a2017b369c55574fab4717cafca51cad2246
interly <0311interly@gmail.com>
2014-05-07 16:08:15 +0800

    modified:   game/panel/handler.js

--- game/panel/handler.js
+++ game/panel/handler.js
@@ -294,6 +294,7 @@ function defend_clicked_handler()
 	var slot = getSlotByXY(from_x, from_y);
 	var army = getArmyById(slot.army_id);
 	var user_action = new action('defend', army.army_id, from_x, from_y, null, null, army.type_id);
+	console.log(army);
 	console.log('defend at (' + from_x+ ', ' + from_y + ')');
 	user_action.get_result();
 	update_result_list_div();
