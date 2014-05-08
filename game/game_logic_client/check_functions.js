af411b25479a22b7fb9cb24c3444961a435f41c8
tony-yi <455054421@qq.com>
2014-05-08 15:15:08 +0800

    coding /game by Tony

--- game/game_logic_client/check_functions.js
+++ game/game_logic_client/check_functions.js
@@ -17,7 +17,7 @@ function check_slot_right_click_able(e) //check the slot owner
 
 	var slot = getSlotByXY(pos_x, pos_y); //in slot.js
 
-	if(parseInt(getArmyById(slot.army_id).owner) == user_id)
+	if(parseInt(getArmyById(slot.army_id).owner) == user_id && IsMyTurn())
 	{
 		return true;
 	}
