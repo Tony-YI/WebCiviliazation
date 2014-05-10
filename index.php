5c8e7dcf69e8fc55053bcb7a6bf2305ab17395b9
Sylvia <sylvia@haha.com>
2014-05-10 13:59:15 +0800

    test

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" type="text/css" href="test.css">
+	
 	<?php
 		#Check Cookie here
 		#if cookie exist, redirect the page
 		#else ask the user to login
-		$cookie = $_COOKIE['USER_COOKIE'];
+		/*$cookie = $_COOKIE['USER_COOKIE'];
 		require_once("lib/db.php");
 		if(check_cookie($db))
 		{
@@ -16,8 +17,9 @@
 		{
 			#redirect to login.php
 			echo "<script type=\"text/javascript\">location.href=\"./login/login.php\"</script>";
-		}
+		}*/
 	?>
+	
 
 	<div><img src="images/cut3.png" class="cut"></div>
 </html>
