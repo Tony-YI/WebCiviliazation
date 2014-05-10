c2a8721a120314bf5b60d9f609532e81122a0ed9
Sylvia <sylvia@haha.com>
2014-05-10 16:27:33 +0800

    test

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 function onclick1()
 {
-	img = document.getElementById('flash');
+	var img = document.getElementById('flash');
 	img.style.display = "block";
 	img.style.width = "100%";
 	img.style.length = "100%";
+	img.style.zIndex = "100";
+	alert(img);
 }
