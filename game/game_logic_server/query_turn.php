68b02c7eb0a7fa7c3ebf82908bd95e18d03bd253
Edward <huangxx_2155@qq.com>
2014-05-11 15:32:47 +0800

    debugging

--- game/game_logic_server/query_turn.php
+++ game/game_logic_server/query_turn.php
@@ -15,7 +15,7 @@
 		exit;
 	}
 	//check if the game is ended
-	if(game_is_over($db,$game_id))
+	if(($game_end = game_is_over($db,$game_id)))
 	{
 		//if so, get the winner's player id
 		$SQL_SELECT_WINNER = "SELECT player_id FROM game_{$game_id}_playerlist WHERE player_status != 2";
@@ -31,12 +31,13 @@
 		$SQL_INSERT_WIN_RESULT = "INSERT INTO game_{$game_id}_resultlist (result_id,action_type,player_id) VALUES ($new_result_id,'win',$row[0])";
 		mysqli_query($SQL_INSERT_WIN_RESULT);
 	}
-	
+
 	$SQL_GET_ACTIVE_PLAYER = "SELECT * FROM game_{$game_id}_playerlist WHERE player_turn = 1";
 	$result = mysqli_query($db,$SQL_GET_ACTIVE_PLAYER);
 	$row = mysqli_fetch_row($result);
-
-	echo "{\"active_player\":\"$row[0]\"";
+	echo "{";
+	echo "\"game_end\":\"$game_end\"";
+	echo ",\"active_player\":\"$row[0]\"";
 	echo ",\"player_gold\":\"$row[2]\"";
 	echo ",\"player_wood\":\"$row[3]\"";
 
