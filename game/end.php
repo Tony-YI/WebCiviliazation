2d7b26d466b398244a4178fb3d95f61ebc25e735
Edward <huangxx_2155@qq.com>
2014-05-11 17:27:46 +0800

    debugging, try to delete the game when the game is over

---
+++ game/end.php
@@ -0,0 +1,48 @@
+<?php
+	require_once("../lib/db.php");
+	require_once("game_logic_server/lib.php");
+	$user_id = $_COOKIE["CURRENT_USER"];
+	$game_id = $_COOKIE["IN_GAME"];
+	
+	if(!check_cookie($db))
+	{
+		echo "{\"status\":\"error\",";
+		echo "\"error_detail\":\"no_valid_cookie\"}";
+		exit;
+	}
+
+	if(game_is_over($db,$game_id))
+	{
+		$SQL_VISITED = "UPDATE game_{$game_id}_playerlist SET player_status = 3 WHERE player_id = $user_id";
+		mysqli_query($db,$SQL_VISITED);
+		$SQL_VISITED_NUM = "SELECT * FROM game_{$game_id}_playerlist WHERE player_status = 3";
+		$result = mysqli_query($db,$SQL_VISITED_NUM);
+		$row_num = mysqli_num_rows($result);
+
+		echo <<<HTML_CONTENT
+<!DOCTYPE html>
+
+<html>
+<head>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.js"></script>
+    <script type="text/javascript" src="/jquery.cookie.js"></script>
+	<script type="text/javascript">
+		function delete_game()
+		{
+			$.removeCookie("IN_GAME");
+			window.location.href = "/room.php";
+		}
+	</script>
+</head>
+<body>
+	<p>$row_num players have known the game is over</p>
+	<p>The game has endded !</p>
+	<p>Thank you for playing !</p>
+	<button type="button" onclick="delete_game()">Click Here to return to room selection</button>
+</body>
+</html>
+HTML_CONTENT;
+		if($row_num >= 3)
+		delete_game($db,$game_id);
+	}
+?>
--- lib/db.php
+++ lib/db.php
@@ -71,4 +71,44 @@ $army_wood_cost = array(20,15,25);
 		}
 	}
 
+function delete_game($con,$game_id)
+{
+	$SQL_DROP_playerlist = "DROP TABLE game_{$game_id}_playerlist";
+	$SQL_DROP_armylist = "DROP TABLE game_{$game_id}_armylist";
+	$SQL_DROP_slotlist = "DROP TABLE game_{$game_id}_slotlist";
+	$SQL_DROP_actionlist = "DROP TABLE game_{$game_id}_resultlist";
+	$SQL_DROP_occupationrecord = "DROP TABLE game_{$game_id}_occupationrecord";
+	$SQL_DELETE_GAME = "DELETE FROM Game WHERE game_id = $game_id";
+	if(!mysqli_query($db,$SQL_DROP_occupationrecord))
+	{
+		$sql_error = mysqli_error($db);
+		echo "\"occupationrecordError\":\"$sql_error\",";
+	}
+	if(!mysqli_query($db,$SQL_DROP_actionlist))
+	{
+		$sql_error = mysqli_error($db);
+		echo "\"actionlistError\":\"$sql_error\",";
+	}
+	if(!mysqli_query($db,$SQL_DROP_slotlist))
+	{
+		$sql_error = mysqli_error($db);
+		echo "\"slotlistError\":\"$sql_error\",";
+	}
+	if(!mysqli_query($db,$SQL_DROP_armylist))
+	{
+		$sql_error = mysqli_error($db);
+		echo "\"armylistError\": $sql_error\",";
+	}
+	if(!mysqli_query($db,$SQL_DROP_playerlist))
+	{
+		$sql_error = mysqli_error($db);
+		echo "\"playerlistError\":\"$sql_error\",";
+	}
+	if(!mysqli_query($db,$SQL_DELETE_GAME))
+	{
+		$sql_error = mysqli_error($db);
+		echo "\"deletegame\":\"$sql_error\",";
+	}
+}
+
 ?>
