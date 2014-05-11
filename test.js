751cc854de1144c88298a3350adae420d1673ebb
Sylvia <sylvia@haha.com>
2014-05-11 15:30:17 +0800

    test in index

--- test.js
+++ test.js
@@ -3,6 +3,9 @@ function onclick1()
 	img1 = document.getElementById('img1');
 	img1.style.display="block";
 	img1.classList.add('change-size');
+            var audio = document.getElementById("attackAudio");
+            audio.src="../../audio/cut.wmv";
+            audio.play();
 }
 
 
