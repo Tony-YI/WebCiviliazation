0f26994501c4b0f3893fcfd4bf78a988ff68063f
Sylvia <sylvia@haha.com>
2014-05-12 14:41:51 +0800

    index modify

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" type="text/css" href="test.css">
-<script type="text/javascript" src="test.js"></script>
 	
 	<?php
 		#Check Cookie here
 		#if cookie exist, redirect the page
 		#else ask the user to login
-		/*$cookie = $_COOKIE['USER_COOKIE'];
+		$cookie = $_COOKIE['USER_COOKIE'];
 		require_once("lib/db.php");
 		if(check_cookie($db))
 		{
@@ -18,12 +16,7 @@
 		{
 			#redirect to login.php
 			echo "<script type=\"text/javascript\">location.href=\"./login/login.php\"</script>";
-		}*/
+		}
 	?>
-	
-	
-	<input type="button" id="button" value="Click" onclick="onclick1()"/>
-	<img src="images/cut3.png" id="img1">
-	<audio id="attackAudio" src="../../audio/cut.wav"></audio>
 
 </html>
